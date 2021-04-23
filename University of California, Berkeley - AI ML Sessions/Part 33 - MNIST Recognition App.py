c383d23d04de8288a2803ea0be5d935e757ed205
Gian Millare <gianmillaregsm@gmail.com>
2021-04-22 22:24:47 -0700

    created the flask app and the model for the MNIST app ucbmlai33

--- University of California, Berkeley - AI ML Sessions/Part 33 - MNIST Recognition App.py
+++ University of California, Berkeley - AI ML Sessions/Part 33 - MNIST Recognition App.py
@@ -1,69 +1,19 @@
+# An app that uses the MNIST Machine Learning Library
+# Objective: Have the app output a number that is displayed on a submitted image
+
+# Step 1: Import Dependencies
 import os
 from flask import Flask, request, jsonify
 
-import keras
+import keras # dependencies for ML
 from keras.preprocessing import image
 from keras import backend as K
 
+# Step 2: create the Flask app
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
 
+# Step 3: create the model by importing a pre-made MNIST model from keras
+model = keras.models.load_model("mnist_trained.h5")
+graph = K.get_session().graph # in tensorflow, all computation must exist in a graph. So we import a graph session from the backend
 
-def prepare_image(img):
-    # Convert the image to a numpy array
-    img = image.img_to_array(img)
-    # Scale from 0 to 255
-    img /= 255
-    # Invert the pixels
-    img = 1 - img
-    # Flatten the image to an array of pixels
-    image_array = img.flatten().reshape(-1, 28 * 28)
-    # Return the processed feature array
-    return image_array
-
-
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
-    data = {"success": False}
-    if request.method == 'POST':
-        print(request)
-
-        if request.files.get('file'):
-            # read the file
-            file = request.files['file']
-
-            # read the filename
-            filename = file.filename
-
-            # create a path to the uploads folder
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-
-            # Save the file to the uploads folder
-            file.save(filepath)
-
-            # Load the saved image using Keras and resize it to the mnist
-            # format of 28x28 pixels
-            image_size = (28, 28)
-            im = image.load_img(filepath, target_size=image_size,
-                                grayscale=True)
-
-            # Convert the 2D image to an array of pixel values
-            image_array = prepare_image(im)
-            print(image_array)
-
-            # Get the tensorflow default graph and use it to make predictions
-
-            return jsonify(data)
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    '''
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
