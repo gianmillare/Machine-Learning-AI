2d93fa7dd59e98eb3fbd5a5e2cc9e46a6cbf0ed3
Gian Millare <gianmillaregsm@gmail.com>
2021-04-22 22:30:56 -0700

    completed the prepare_image function in ucbmlai33

--- University of California, Berkeley - AI ML Sessions/Part 33 - MNIST Recognition App.py
+++ University of California, Berkeley - AI ML Sessions/Part 33 - MNIST Recognition App.py
@@ -17,3 +17,19 @@ app.config['UPLOAD_FOLDER'] = 'uploads'
 model = keras.models.load_model("mnist_trained.h5")
 graph = K.get_session().graph # in tensorflow, all computation must exist in a graph. So we import a graph session from the backend
 
+# Step 4: create a function that prepares the image. This should mimic the data preprocessing in ucbmlai32
+def prepare_image(img):
+    
+    # Convert the image into a numpy array
+    img = image.img_to_array(img)
+
+    # Scale the image (remember that pixels are 0-255, so we scale it by dividing 255)
+    img /= 255
+
+    # Invert the pixels (because the pixes are currently inverted and will pop up as opposite colors (white number with black background))
+    img = 1 - img
+
+    # 'flatten' the image and reshape it using the 28*28 (this is unique only to MNIST database)
+    image_array = img.flatten().reshape(-1, 28 * 28)
+
+    # return the processed image array (because the output will be passed into another function)
